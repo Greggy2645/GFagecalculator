dcd198362a35756b6aabf0b80eb969bcc52a2093
Greggy2645 <64741164+Greggy2645@users.noreply.github.com>
2020-05-04 23:22:15 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+#ik ga hier morgen wel mee verder
+def calculator():
+    eigen_leeftijd = int(input("wat is jouw eigen leeftijd?\n"))
+    minimale_leeftijd = (eigen_leeftijd / 2)+7
+    print("minimale leeftijd voor relatie is:")
+    print(minimale_leeftijd)
