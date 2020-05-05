914ddfb90ca7c3e4990a92ccc1829d4a00bccee1
Terebo <63092966+Terebo@users.noreply.github.com>
2020-05-05 08:30:45 +0200

    added JSON
    added some stuff

--- calculator.py
+++ calculator.py
@@ -1,6 +1,28 @@
-#ik ga hier morgen wel mee verder
 def calculator():
+    import random
     eigen_leeftijd = int(input("wat is jouw eigen leeftijd?\n"))
     minimale_leeftijd = (eigen_leeftijd / 2)+7
-    print("minimale leeftijd voor relatie is:")
-    print(minimale_leeftijd)
+    #haal de json op
+    with open('responses.json') as f:
+        data = json.load(f)
+    #genereer random getal
+    num1 = random.randint(0, 5)
+    #wacht 1.5 seconden
+    print("\r.", end='\r')
+    time.sleep(0.5)
+    print("\r.", end='\r')
+    time.sleep(0.5)
+    print("\r.")
+    #antwoord
+    print("minimale leeftijd voor relatie is: " + str(minimale_leeftijd))
+    #print een response die random nummer in de array van de JSON is
+    print (data[num1])
+    #wacht 0.5 seconden totdat ie weer door gaat
+    time.sleep(0.5)
+    calculator()
+import time
+import json
+import random
+import sys
+calculator()
+
